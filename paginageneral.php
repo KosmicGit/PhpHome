25bd15f152575091a7150bc63f730fc2a3b12f7d
Estresban <101512042+Estresban@users.noreply.github.com>
2022-04-12 14:24:01 +0200

    Update paginageneral.php

--- paginageneral.php
+++ paginageneral.php
@@ -1,6 +1,20 @@
 <?php
 	include("conexion.php");
-
+	//La siguiente linea configura el modo de conexion a el puerto serial y 9600 baudios
+	//exec("mode /dev/ttyACM0 BAUD=9600 PARITY=N data=8 stop=1 xon=off");//para linux
+	//$puerto = "/dev/ttyACM0";
+	exec("mode COM4 BAUD=9600 PARITY=N data=8 stop=1 xon=off");//para windows
+        $puerto ="COM4";
+        $fp = fopen ($puerto, "w+");
+        if (!$fp) {$status = "Conexion FALLIDA en el puerto $puerto";} 
+	else {$status = "Conexion OK en el puerto $puerto";}
+        echo $status;
+	sleep(1);
+	$a='1';
+	fwrite($fp,$a);
+	sleep(1);
+	echo "<br>comando $a";
+	fclose($fp);
 ?>
 <!DOCTYPE HTML>
 <head>
